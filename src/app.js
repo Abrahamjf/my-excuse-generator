e28283a63343911d97d55d1c3494aff463840216
Abrahamjf <abrahamjferrer@gmail.com>
2022-09-04 16:28:47 +0000

    :hammer: Se completo el generador de excusas

--- src/app.js
+++ src/app.js
@@ -6,6 +6,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let excuseGenerator = () => {
+    let whos = ["The dog", "My grandma", "His turtle", "My bird", "my fish"];
+    let whats = ["ate", "peed", "crushed", "broke", "sold"];
+    let tos = [
+      "my homework",
+      "the keys",
+      "the car",
+      "my house",
+      "my dental prosthesis"
+    ];
+    let whens = [
+      "before the class",
+      "right on time",
+      "at the end of the day",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    let who = whos[Math.floor(Math.random() * whos.length)];
+    let what = whats[Math.floor(Math.random() * whats.length)];
+    let to = tos[Math.floor(Math.random() * tos.length)];
+    let when = whens[Math.floor(Math.random() * whens.length)];
+
+    return who + " " + what + " " + to + " " + when;
+  };
+
+  document.querySelector("#excuse").innerHTML = excuseGenerator();
 };
